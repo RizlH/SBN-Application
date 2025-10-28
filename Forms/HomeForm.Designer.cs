d7c0212c24342e560850e95e2d8b5d026f821746
RizlH <rzlhrmwn00@gmail.com>
2025-10-28 15:27:41 +0700

    Menambahkan folder & beberapa files

---
+++ Forms/HomeForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace SBN_Application.Forms
+{
+    partial class HomeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            homeToolStripMenuItem = new ToolStripMenuItem();
+            buyerToolStripMenuItem = new ToolStripMenuItem();
+            sbnToolStripMenuItem = new ToolStripMenuItem();
+            assetToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.BackColor = SystemColors.ActiveBorder;
+            menuStrip1.ImageScalingSize = new Size(24, 24);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { homeToolStripMenuItem, buyerToolStripMenuItem, sbnToolStripMenuItem, assetToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(878, 33);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // homeToolStripMenuItem
+            // 
+            homeToolStripMenuItem.BackColor = Color.White;
+            homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            homeToolStripMenuItem.Size = new Size(77, 29);
+            homeToolStripMenuItem.Text = "Home";
+            // 
+            // buyerToolStripMenuItem
+            // 
+            buyerToolStripMenuItem.BackColor = Color.White;
+            buyerToolStripMenuItem.Name = "buyerToolStripMenuItem";
+            buyerToolStripMenuItem.Size = new Size(72, 29);
+            buyerToolStripMenuItem.Text = "Buyer";
+            // 
+            // sbnToolStripMenuItem
+            // 
+            sbnToolStripMenuItem.BackColor = Color.White;
+            sbnToolStripMenuItem.Name = "sbnToolStripMenuItem";
+            sbnToolStripMenuItem.Size = new Size(61, 29);
+            sbnToolStripMenuItem.Text = "SBN";
+            // 
+            // assetToolStripMenuItem
+            // 
+            assetToolStripMenuItem.BackColor = Color.White;
+            assetToolStripMenuItem.Name = "assetToolStripMenuItem";
+            assetToolStripMenuItem.Size = new Size(71, 29);
+            assetToolStripMenuItem.Text = "Asset";
+            // 
+            // HomeForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(878, 544);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "HomeForm";
+            Text = "HomeForm";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem homeToolStripMenuItem;
+        private ToolStripMenuItem buyerToolStripMenuItem;
+        private ToolStripMenuItem sbnToolStripMenuItem;
+        private ToolStripMenuItem assetToolStripMenuItem;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using SBN_Application.Forms;
+
 namespace SBN_Application
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace SBN_Application
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new HomeForm());
         }
     }
 }
