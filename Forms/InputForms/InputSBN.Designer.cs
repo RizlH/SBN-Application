ffe6f528858927dde73adf3deb8d9e770f24e1f2
dynda21 <putridinda2134>
2025-10-31 13:09:58 +0700

    Update SBN page

--- Forms/InputForms/InputSBN.Designer.cs
+++ Forms/InputForms/InputSBN.Designer.cs
@@ -28,12 +28,164 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "InputSBN";
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            label5 = new Label();
+            textBox5 = new TextBox();
+            button1 = new Button();
+            label6 = new Label();
+            textBox6 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(45, 42);
+            label1.Name = "label1";
+            label1.Size = new Size(97, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Nama SBN";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(45, 113);
+            label2.Name = "label2";
+            label2.Size = new Size(91, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Kode SBN";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(45, 397);
+            label3.Name = "label3";
+            label3.Size = new Size(104, 25);
+            label3.TabIndex = 2;
+            label3.Text = "Fixed Rated";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(214, 36);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(232, 31);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(214, 107);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(232, 31);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(214, 176);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(232, 31);
+            textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(45, 182);
+            label4.Name = "label4";
+            label4.Size = new Size(84, 25);
+            label4.TabIndex = 6;
+            label4.Text = "Deskripsi";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(214, 248);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(232, 31);
+            textBox4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(45, 329);
+            label5.Name = "label5";
+            label5.Size = new Size(161, 25);
+            label5.TabIndex = 8;
+            label5.Text = "Minimal Pembelian";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(214, 323);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(232, 31);
+            textBox5.TabIndex = 9;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(214, 456);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 10;
+            button1.Text = "Tambah";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(45, 251);
+            label6.Name = "label6";
+            label6.Size = new Size(114, 25);
+            label6.TabIndex = 11;
+            label6.Text = "Tipe Investor";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(214, 391);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(232, 31);
+            textBox6.TabIndex = 12;
+            // 
+            // InputSBN
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(866, 512);
+            Controls.Add(textBox6);
+            Controls.Add(label6);
+            Controls.Add(button1);
+            Controls.Add(textBox5);
+            Controls.Add(label5);
+            Controls.Add(textBox4);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "InputSBN";
+            Text = "InputSBN";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label4;
+        private TextBox textBox4;
+        private Label label5;
+        private TextBox textBox5;
+        private Button button1;
+        private Label label6;
+        private TextBox textBox6;
     }
 }
--- Forms/Menus/SBNPage.Designer.cs
+++ Forms/Menus/SBNPage.Designer.cs
@@ -28,18 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            dataGridViewsbn = new DataGridView();
+            inputsbnbtn = new Button();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewsbn).BeginInit();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(43, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(102, 25);
+            label1.TabIndex = 0;
+            label1.Text = "SBN PAGES";
+            // 
+            // dataGridViewsbn
+            // 
+            dataGridViewsbn.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewsbn.Location = new Point(43, 81);
+            dataGridViewsbn.Name = "dataGridViewsbn";
+            dataGridViewsbn.RowHeadersWidth = 62;
+            dataGridViewsbn.Size = new Size(912, 382);
+            dataGridViewsbn.TabIndex = 1;
+            // 
+            // inputsbnbtn
+            // 
+            inputsbnbtn.Location = new Point(824, 21);
+            inputsbnbtn.Name = "inputsbnbtn";
+            inputsbnbtn.Size = new Size(112, 34);
+            inputsbnbtn.TabIndex = 2;
+            inputsbnbtn.Text = "Input";
+            inputsbnbtn.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(669, 21);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 3;
+            button1.Text = "Delete";
+            button1.UseVisualStyleBackColor = true;
             // 
             // SBNPage
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(227, 253, 253);
+            Controls.Add(button1);
+            Controls.Add(inputsbnbtn);
+            Controls.Add(dataGridViewsbn);
+            Controls.Add(label1);
             Name = "SBNPage";
             Size = new Size(1000, 500);
+            ((System.ComponentModel.ISupportInitialize)dataGridViewsbn).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private DataGridView dataGridViewsbn;
+        private Button inputsbnbtn;
+        private Button button1;
     }
 }
